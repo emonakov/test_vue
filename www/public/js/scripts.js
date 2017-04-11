c8add561f4054511fab5e559ea5cc658d163d767
Eugene <emonakov@robofirm.com>
2017-04-11 07:45:39 -0500

    Recipe filter + Recipe list without pagination

--- www/public/js/scripts.js
+++ www/public/js/scripts.js
@@ -9,10 +9,16 @@ var application = function ($) {
                     '<p><strong>Cooking time: {{ recipe.time }}</strong></p>' +
                     '<p><strong>Ingredients:</strong></p>' +
                     '<ul>' +
-                        '<li v-for="ingredient in recipe.ingredients" v-once>{{ ingredient.name }}</li>' +
+                        '<li v-for="ingredient in recipe.ingredients">{{ ingredient.name }}</li>' +
                     '</ul>' +
                 '</a>' +
             '</div>'
+        });
+
+        // Vue component to print out recipe time option list
+        Vue.component('recipe-time-option', {
+            props: ['recipe'],
+            template: '<option :value="recipe.time">{{ recipe.time }} minutes</option>'
         });
 
         // Vue application which drives the recipe pages
@@ -23,7 +29,12 @@ var application = function ($) {
                 items:[],
                 limit: null,
                 offset: null,
-                total: null
+                total: null,
+                filter: {
+                    main: null,
+                    ingredient: null,
+                    time: null
+                }
             },
             created: function () {
                 this.fetchData();
@@ -31,12 +42,51 @@ var application = function ($) {
             methods: {
                 fetchData: function () {
                     // getting data from rest api endpoint
-                    $.getJSON('/recipes', function (recipes) {
-                        this.items = recipes['items'];
-                        this.limit = recipes['limit'];
-                        this.offset = recipes['offset'];
-                        this.total = recipes['total'];
-                    }.bind(this));
+                    $.getJSON('/recipes', this.renderResponse.bind(this));
+                },
+                filterData: function () {
+                    // filter request
+                    var qs;
+                    var recipeName = this.filter.main;
+                    var ingredientName = this.filter.ingredient;
+                    var time = this.filter.time;
+                    var recipeQuery = (recipeName) ? 'main_table[field]=name&main_table[value]=%' + encodeURIComponent(recipeName) +  '%&main_table[op]=like' : null;
+                    var ingredientQuery = (ingredientName) ? 'ingridient_table[field]=name&ingridient_table[value]=%' + encodeURIComponent(ingredientName) +  '%&ingridient_table[op]=like' : null;
+                    var timeQuery = (time) ? 'main_table[field]=time&main_table[value]=' + encodeURIComponent(time) : null;
+                    if (recipeQuery) {
+                        qs = recipeQuery;
+                        if (ingredientQuery) {
+                            qs += '&' + ingredientQuery;
+                        }
+                        if (timeQuery) {
+                            qs += '&' + timeQuery;
+                        }
+                    } else if (ingredientQuery) {
+                        qs = ingredientQuery;
+                        if (timeQuery) {
+                            qs += '&' + timeQuery;
+                        }
+                    } else if (timeQuery) {
+                        qs = timeQuery;
+                    }
+                    $.getJSON('/recipes?'+qs, this.renderResponse.bind(this));
+                },
+                renderResponse: function (recipes) {
+                    this.items = recipes['items'];
+                    this.limit = recipes['limit'];
+                    this.offset = recipes['offset'];
+                    this.total = recipes['total'];
+                },
+                buildTimeFilterData: function () {
+                    var filterData = [];
+                    for (var key in this.items) {
+                        if (this.items.hasOwnProperty(key)) {
+                            if (filterData.indexOf(this.items[key].time) < 0) {
+                                filterData.push(this.items[key].time);
+                            }
+                        }
+                    }
+                    return filterData;
                 }
             }
         });
